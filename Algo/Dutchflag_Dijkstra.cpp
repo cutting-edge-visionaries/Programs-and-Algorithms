e4b977c520db4fb90118cd511b570081a5ee54b3
SANJIT ANAND <59444668+sanjitcodes@users.noreply.github.com>
2020-10-02 02:02:11 +0530

    added DNF Sort

---
+++ Algo/Dutchflag_Dijkstra.cpp
@@ -0,0 +1,39 @@
+// DNF SORT -- To sort the array of containing only 0,1,2, in a single iteration..
+// Contributer : Sanjit Anand
+#include <iostream>
+using namespace std;
+
+void dnf_sort(int ar[], int n){
+	int high = n-1;
+	int low = 0;              /// 3 pointer approach ...
+	int mid = 0;
+
+	while(mid <= high)
+	{
+		if(ar[mid] == 1)
+		{
+			mid++;
+		}
+		else if(ar[mid] == 2)
+		{
+			swap(ar[mid],ar[high]);
+			high--;
+		}
+		else
+		{
+			swap(ar[mid],ar[low]);
+			mid++;
+			low++;
+		}
+
+	}
+}
+	
+int main(){
+	int ar[]={1,0,0,2,2,1,0,2,0,1,1,0,2};
+	int n = sizeof(ar)/sizeof(int);
+	dnf_sort(ar,n);
+	for(int i=0; i<n; i++){
+		cout << ar[i] << " ";
+	}
+}
